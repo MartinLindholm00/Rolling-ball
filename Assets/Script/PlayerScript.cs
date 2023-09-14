1831f259a6b1a91766a861d13c5c730cbe561392
MartinLindholm00 <martin@mulleland.dk>
2023-09-14 15:25:47 +0200

    created the camera movement

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
+    public float speedForce = 1.0f;
+
+    private float movementX;
+    private float movementY;
+
+    public Rigidbody RB;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+        RB.AddForce(movement * speedForce);
+    }
+
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
 }
